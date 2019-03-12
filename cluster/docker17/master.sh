c138b694b25d1dabae67da15da1ae185062b13ad
newtonjose <josephnewton001@gmail.com>
2019-03-12 16:33:18 -0300

    finish contiv release

--- cluster/docker17/master.sh
+++
@@ -1,5 +0,0 @@
-docker swarm init --advertise-addr $1
-docker swarm join-token manager |
-	grep -A 20 "docker swarm join" >$2/manager.sh
-docker swarm join-token worker |
-	grep -A 20 "docker swarm join" >$2/worker.sh
--- scripts/build_image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-VERSION=${BUILD_VERSION-"latest"}
-
-# Build the docker container for ansible installation
-ansible_spec=./install/ansible/Dockerfile
-docker build -t contiv/install:$VERSION -f $ansible_spec .
